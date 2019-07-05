9055a28c3e28eb39652e10781589d8fcad518899
joelbiffin <joel_biffin@yahoo.co.uk>
2019-07-05 11:50:01 +0100

    Test cases written for DirectedGraphHandler using py.unittest module.

--- src/directed_graph.py
+++ src/directed_graph.py
@@ -4,11 +4,19 @@ from typing import *
 class DirectedGraphHandler(object):
 
     def __init__(self, user_input: str):
-        """Initialises empty directed graph,
-        builds graph from user input string"""
+        """
+        Initialises empty directed graph, builds graph from user input string.
+
+        :param user_input: string representation of jobs and their dependencies
+        """
         self.graph = {}
 
     def topological_sort(self) -> List[str]:
+        """
+        Order the instance's graph dictionary as a string into topological sort.
+
+        :return: list of jobs from graph in topological sort
+        """
         out_list = []
         # TODO: topologically sort directed graph
         return out_list
@@ -19,19 +27,35 @@ class DirectedGraphHandler(object):
 
     @staticmethod
     def _add_trailing_comma(string: str) -> str:
+        """Appends trailing comma to string if doesn't already exist."""
         if string.rstrip().endswith(","):
             return string
         else:
             return string + ","
 
     @staticmethod
-    def _parse_user_input(user_input: str) -> List[tuple]:
-        rows = [()]
-        # TODO: turn string user input into list of tuples
-        return rows
-
-    @staticmethod
     def _build_graph(nodes: List[tuple]) -> dict:
+        """
+        Builds instance's graph hashtable from pre-parsed list of tuples. Deals
+        with self-dependencies, duplicate job definitions and jobs dependent on
+        those which do not exist.
+
+        :param nodes: pre-parsed list of tuples of the form (job, dependency)
+        :return: hashtable representing the graph of jobs and dependencies
+        """
         temp_graph = {}
         # TODO: build graph from list of tuples
         return temp_graph
+
+    @staticmethod
+    def _parse_user_input(user_input: str) -> List[tuple]:
+        """
+        Handles string input and converts it to list representation of jobs and
+        dependencies. Deals only with syntactic errors in input string.
+
+        :param user_input: string representation of jobs and their dependencies
+        :return: list of tuples containing of the form (job, dependency)
+        """
+        rows = [()]
+        # TODO: turn string user input into list of tuples
+        return rows
--- tests/test_directed_graph.py
+++ tests/test_directed_graph.py
@@ -1,7 +1,114 @@
 from unittest import TestCase
-
 from src.directed_graph import DirectedGraphHandler
 
+
 class TestDirectedGraphHandler(TestCase):
-    pass
+
+    def test_parse_user_input(self):
+        # valid user input, no dependencies
+        input1 = "a => , b => , c => "
+        result1 = DirectedGraphHandler._parse_user_input(input1)
+        expected_output1 = [("a", ""), ("b", ""), ("c", "")]
+        self.assertEqual(expected_output1, result1)
+
+        # valid user input, dependency
+        input2 = "a => b, b =>"
+        result2 = DirectedGraphHandler._parse_user_input(input2)
+        expected_output2 = [("a", "b"), ("b", "")]
+        self.assertEqual(expected_output2, result2)
+
+        # invalid user input, absence of '=>'
+        input3 = "a, b =>"
+        self.assertRaises(IOError,
+                          DirectedGraphHandler._parse_user_input, input3)
+
+        # invalid user input, absence of '=>'
+        input4 = "a"
+        self.assertRaises(IOError,
+                          DirectedGraphHandler._parse_user_input, input4)
+
+    def test_add_trailing_comma(self):
+        # needs appended comma
+        input1 = "a,b,c"
+        result1 = DirectedGraphHandler._add_trailing_comma(input1)
+
+        # does not need appended comma
+        input2 = "a,b,c,"
+        result2 = DirectedGraphHandler._add_trailing_comma(input2)
+
+        expected_output = "a,b,c,"
+
+        self.assertEqual(expected_output, result1)
+        self.assertEqual(expected_output, result2)
+
+    def test_build_graph(self):
+        # valid graph with no dependencies
+        input1 = [("a", ""), ("b", ""), ("c", "")]
+        result1 = DirectedGraphHandler._build_graph(input1)
+        expected_output1 = {"a": "", "b": "", "c": ""}
+        self.assertEqual(expected_output1, result1)
+
+        # valid graph with dependency
+        input2 = [("a", "b"), ("b", "c"), ("c", "")]
+        result2 = DirectedGraphHandler._build_graph(input2)
+        expected_output2 = {"a": "b", "b": "c", "c": ""}
+        self.assertEqual(expected_output2, result2)
+
+        # duplicate jobs in list, throw IOError
+        input3 = [("a", ""), ("a", "")]
+        self.assertRaises(IOError,
+                          DirectedGraphHandler._build_graph, input3)
+
+        # self loop in list item, throw IOError
+        input4 = [("a", "a")]
+        self.assertRaises(IOError,
+                          DirectedGraphHandler._build_graph, input4)
+
+        # job depends on job which does not exist, throw IOError
+        input5 = [("a", "f")]
+        self.assertRaises(IOError,
+                          DirectedGraphHandler._build_graph, input5)
+
+    def test_topological_sort(self):
+        handler = DirectedGraphHandler("")
+
+        # graph with only one job, no dependencies
+        handler.graph = {"a": ""}
+        result1 = handler.topological_sort()
+        expected_output1 = ["a"]
+        self.assertEqual(expected_output1, result1)
+
+        handler.graph.clear()
+
+        # graph with multiple jobs, no dependencies
+        handler.graph = {"a": "", "b": "", "c": ""}
+        result2 = handler.topological_sort()
+        expected_outputs2 = {
+            ["a", "b", "c"], ["b", "c", "a"], ["c", "a", "b"],
+            ["a", "c", "b"], ["c", "b", "a"], ["b", "a", "c"]
+        }
+        self.assertIn(result2, expected_outputs2)
+
+        handler.graph.clear()
+
+        # graph with multiple jobs, one dependency
+        handler.graph = {"a": "", "b": "c", "c": ""}
+        result3 = handler.topological_sort()
+        expected_outputs3 = {["c", "b", "a"], ["c", "a", "b"]}
+        self.assertIn(result3, expected_outputs3)
+
+        handler.graph.clear()
+
+        # graph with multiple dependencies
+        handler.graph = {
+            "a": "", "b": "c", "c": "f", "d": "a", "e": "b", "f": ""
+        }
+        result4 = handler.topological_sort()
+        expected_outputs4 = {
+            ["f", "c", "b", "a", "d", "e"],
+            ["f", "c", "b", "a", "e", "f"]
+        }
+        self.assertIn(result4, expected_outputs4)
+
+
 
